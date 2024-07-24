b1d230edf06cb7c4fe3f82b3034ad923d67288ac
qstokes <quincy.f.stokes@gmail.com>
2024-07-24 01:44:45 -0700

    Crafting Tab
    crafting tab stuff

--- Assets/CraftingMenu.cs
+++ Assets/CraftingMenu.cs
@@ -1,14 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CraftingMenu : MonoBehaviour
 {
     public List<GameObject> contentMenus;
-    public void ChangeCraftingTab(GameObject menu) {
-        foreach (GameObject go in contentMenus) {
-            go.SetActive(false);
+    public List<GameObject> craftingTabs;
+    [SerializeField] private Sprite deactivatedImage;
+    [SerializeField] private Sprite activatedImage;
+    public void ChangeCraftingTab(int num) {
+        
+        for(int i = 0; i < contentMenus.Count; ++i) {
+            contentMenus[i].SetActive(false);
+            craftingTabs[i].GetComponent<Image>().sprite = deactivatedImage;
+            craftingTabs[i].transform.localScale = new Vector3(.75f, .75f, 1f);
         }
-        menu.SetActive(true);
+        contentMenus[num].SetActive(true);
+        craftingTabs[num].GetComponent<Image>().sprite = activatedImage;
+        craftingTabs[num].transform.localScale = new Vector3(1.5f, 1.5f, 1f);
+
+
+       
+        //menu. transform.Image.sprite = activatedImage;
     }
+
+
+
 }
