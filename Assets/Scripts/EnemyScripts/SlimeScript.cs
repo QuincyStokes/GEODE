8134d98eb160d0657bb0dedecfc553173cd3e3b6
qstokes <quincy.f.stokes@gmail.com>
2024-08-08 11:34:01 -0700

    Damage Tweak, large tile?

--- Assets/Scripts/EnemyScripts/SlimeScript.cs
+++ Assets/Scripts/EnemyScripts/SlimeScript.cs
@@ -7,9 +7,33 @@ public class EnemyScript : MonoBehaviour
 
     Animator animationManager;
     public AudioSource slimeJump; 
+    public GameObject player;
+    public float moveSpeed;
+    private Rigidbody2D rb;
+    private ContactFilter2D movementFilter;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
+    public float collisionOffset = 0.05f;
 
     public void Start() {
         animationManager = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        player = GameObject.Find("Player");
+    }
+
+    public void Update() {
+        // Calculate the direction from the enemy to the player
+        Vector2 direction = (player.transform.position - transform.position).normalized;
+
+        // Move the enemy towards the player
+        bool success = TryMove(direction);
+                if(!success) {
+                    success = TryMove(new Vector2(direction.x, 0));
+
+                    if(!success) {
+                        success = TryMove(new Vector2(0, direction.y));
+                    }
+                }
+       
     }
     public float Health{
         set {
@@ -31,5 +55,16 @@ public class EnemyScript : MonoBehaviour
 
     public void RemoveEnemy() {
         Destroy(gameObject);
+    }
+
+    private bool TryMove(Vector2 direction) {
+        int count = rb.Cast(direction, movementFilter, castCollisions, moveSpeed * Time.fixedDeltaTime + collisionOffset);
+        if(count == 0) {
+            rb.MovePosition(rb.position + direction * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        } 
+        //Debug.Log("NOPE");
+        return false;
+
     }
 }
